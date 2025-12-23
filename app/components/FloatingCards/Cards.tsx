0c44d918f8f500a4d259c5456adc7cef62c250b9
IshimweArlene <ishimwearlene74@gmail.com>
2025-12-23 20:06:42 +0200

    changes

--- app/components/FloatingCards/Cards.tsx
+++ app/components/FloatingCards/Cards.tsx
@@ -9,8 +9,9 @@ const Cards = () => {
   const [active, setActive] = useState<"day" | "week" | "month">("day");
 
   return (
-    <div>
-      <div className="w-96 bg-[rgba(252,252,232,1)] pr-6 rounded-2xl">
+    <div className="relative flex gap-4 h-[600px] overflow-visible">
+      {/* First Card */}
+      <div className="w-96 absolute z-30 bg-[rgba(252,252,232,1)] mt-24 pr-6 rounded-2xl shadow-lg transform rotate-[-2deg]">
         <div className="flex items-center pt-6 pb-4 px-6">
           <div className="border-[rgba(252,79,36,1)] bg-[rgba(252,79,36,0.08)] border-4 w-5 h-5 rounded-full"></div>
           <h1 className="text-[26px] font-black ml-2">Task Progress</h1>
@@ -22,7 +23,7 @@ const Cards = () => {
           </p>
         </div>
 
-        <div className="bg-white p-3 rounded-r-2xl">
+        <div className="bg-white p-3 rounded-r-2xl rounded-bl-2xl">
           <div className="flex justify-between">
             <div className="flex items-center">
               <ChevronDown fill="black" size={18} className="rotate-270" />
@@ -30,25 +31,11 @@ const Cards = () => {
             </div>
 
             <div className="bg-gray-300 w-fit flex items-center p-2 gap-3 rounded-xl">
-              <button
-                onClick={() => setActive("day")}
-                className={
-                  active === "day"
-                    ? "text-black font-semibold bg-white px-2 rounded-md"
-                    : "text-gray-400"
-                }
-              >
+              <button onClick={() => setActive("day")} className={ active === "day" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400" } >
                 Day
               </button>
 
-              <button
-                onClick={() => setActive("week")}
-                className={
-                  active === "week"
-                    ? "text-black font-semibold bg-white px-2 rounded-md"
-                    : "text-gray-400"
-                }
-              >
+              <button onClick={() => setActive("week")} className={ active === "week" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400"}>
                 Week
               </button>
 
@@ -93,7 +80,7 @@ const Cards = () => {
             <div className="flex-1 h-[0.5px] bg-gray-300"></div>
           </div>
 
-          <div className="flex ml-32 -mt-8 border-[rgba(167,195,209,1)] border-2 items-center gap-4 w-40 py-2 px-2 rounded-xs bg-[rgba(225,251,254,1)]">
+          <div className="flex ml-36 -mt-8 border-[rgba(167,195,209,1)] border-2 items-center gap-4 w-40 py-2 px-2 rounded-xs bg-[rgba(225,251,254,1)]">
             <div>
               <h1 className="text-[8px]">Fintask</h1>
               <h1 className="text-xs font-black">Ux copywrite</h1>
@@ -106,12 +93,12 @@ const Cards = () => {
             </div>
           </div>
 
-          <div className="flex items-center gap-4 pr-2 mt-6">
+          <div className="flex items-center gap-4 pr-2 mt-12">
             <h1 className="text-gray-400 text-xs">12:00 PM</h1>
             <div className="flex-1 h-[0.5px] bg-gray-300"></div>
           </div>
 
-          <div className="w-52 bg-white absolute z-30 rounded-xl py-4 px-4 space-y-3 shadow-xs border-gray-300">
+          <div className="w-[172px] ml-24 -mt-12 bg-[rgba(253,254,225,1)] rounded-xl py-2 px-2 space-y-1  border border-[rgba(248,255,31,1)]">
             <div className="flex items-center gap-12 justify-between">
               <p className="font-extralight text-gray-400 text-[8px]">Fintask</p>
               <MoreHorizontal />
@@ -128,15 +115,280 @@ const Cards = () => {
               <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
             </div>
 
-              <div className="flex">
-                <span className="text-[9px] rounded-sm bg-gray-300 p-1 font-serif">Visual</span>
-                <span className="text-[9px] rounded-sm bg-gray-300 p-1 font-serif mx-2">🏆</span>
-                <p className="mr-1"><MessageCircle size={10} color="gray" /></p>
-                <p className="text-xs text-gray-500 mr-4">5</p>
-                <p className="mr-1"><Paperclip size={10} color="gray" /></p>
-                <p className="text-xs text-gray-500">3</p>
+              <div className="flex items-center">
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif">Visual</span>
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif ml-0.5 mr-3">🏆</span>
+                <p><MessageCircle size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500 mr-2">5</p>
+                <p><Paperclip size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500">3</p>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[rgba(0,0,0,0.03)] w-80 h-[395px] rounded-2xl absolute z-10 top-48 -left-8 -rotate-6"></div>
+      <div className="w-96 absolute z-30 bg-[rgba(252,252,232,1)] mt-24 pr-6 rounded-2xl">
+        <div className="flex items-center pt-6 pb-4 px-6">
+          <div className="border-[rgba(252,79,36,1)] bg-[rgba(252,79,36,0.08)] border-4 w-5 h-5 rounded-full"></div>
+          <h1 className="text-[26px] font-black ml-2">Task Progress</h1>
+        </div>
+
+        <div>
+          <p className="text-[19px] text-start text-gray-800 ml-12 mb-3.5">
+            Send scheduling links guests love
+          </p>
+        </div>
+
+        <div className="bg-white p-3 rounded-r-2xl">
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <ChevronDown fill="black" size={18} className="rotate-270" />
+              <p className="text-[13px] font-bold"> November 15</p>
+            </div>
+
+            <div className="bg-gray-300 w-fit flex items-center p-2 gap-3 rounded-xl">
+              <button onClick={() => setActive("day")} className={ active === "day" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400" } >
+                Day
+              </button>
+
+              <button onClick={() => setActive("week")} className={ active === "week" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400"}>
+                Week
+              </button>
+
+              <button
+                onClick={() => setActive("month")}
+                className={
+                  active === "month"
+                    ? "text-black font-semibold bg-white px-2 rounded-md"
+                    : "text-gray-400"
+                }
+              >
+                Month
+              </button>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-6">
+            <h1 className="text-gray-400 text-xs">08:00 AM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="flex ml-[66px] -mt-2 border-[rgba(243,176,255,1)] border-2 items-center gap-4 w-40 py-4 px-2 rounded-md bg-[rgba(252,233,255,1)]">
+            <div>
+              <h1 className="text-[8px]">Fintask</h1>
+              <h1 className="text-xs font-black">Developement</h1>
+            </div>
+
+            <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img
+                src={image2.src}
+                className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3"
+              />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3">
+                3+
+              </span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-6">
+            <h1 className="text-gray-400 text-xs">10:00 AM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="flex ml-36 -mt-8 border-[rgba(167,195,209,1)] border-2 items-center gap-4 w-40 py-2 px-2 rounded-xs bg-[rgba(225,251,254,1)]">
+            <div>
+              <h1 className="text-[8px]">Fintask</h1>
+              <h1 className="text-xs font-black">Ux copywrite</h1>
+            </div>
+
+            <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-12">
+            <h1 className="text-gray-400 text-xs">12:00 PM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="w-[172px] ml-24 -mt-12 bg-[rgba(253,254,225,1)] rounded-xl py-2 px-2 space-y-1  border border-[rgba(248,255,31,1)]">
+            <div className="flex items-center gap-12 justify-between">
+              <p className="font-extralight text-gray-400 text-[8px]">Fintask</p>
+              <MoreHorizontal />
+            </div>
+
+            <div>
+              <h1 className="font-black text-xs">Web Visual Design</h1>
+            </div>
+
+            <div className="flex items-center gap-4 mt-2 justify-between">
+              <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
+            </div>
+
+              <div className="flex items-center">
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif">Visual</span>
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif ml-0.5 mr-3">🏆</span>
+                <p><MessageCircle size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500 mr-2">5</p>
+                <p><Paperclip size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500">3</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[rgba(0,0,0,0.03)] w-80 h-[395px] rounded-2xl absolute z-10 top-48 -left-8 -rotate-6"></div>
+      <div className="w-96 absolute z-30 bg-[rgba(252,252,232,1)] mt-24 pr-6 rounded-2xl">
+        <div className="flex items-center pt-6 pb-4 px-6">
+          <div className="border-[rgba(252,79,36,1)] bg-[rgba(252,79,36,0.08)] border-4 w-5 h-5 rounded-full"></div>
+          <h1 className="text-[26px] font-black ml-2">Task Progress</h1>
+        </div>
+
+        <div>
+          <p className="text-[19px] text-start text-gray-800 ml-12 mb-3.5">
+            Send scheduling links guests love
+          </p>
+        </div>
+
+        <div className="bg-white p-3 rounded-r-2xl">
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <ChevronDown fill="black" size={18} className="rotate-270" />
+              <p className="text-[13px] font-bold"> November 15</p>
+            </div>
+
+            <div className="bg-gray-300 w-fit flex items-center p-2 gap-3 rounded-xl">
+              <button onClick={() => setActive("day")} className={ active === "day" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400" } >
+                Day
+              </button>
+
+              <button onClick={() => setActive("week")} className={ active === "week" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400"}>
+                Week
+              </button>
+
+              <button
+                onClick={() => setActive("month")}
+                className={
+                  active === "month"
+                    ? "text-black font-semibold bg-white px-2 rounded-md"
+                    : "text-gray-400"
+                }
+              >
+                Month
+              </button>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-6">
+            <h1 className="text-gray-400 text-xs">08:00 AM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="flex ml-[66px] -mt-2 border-[rgba(243,176,255,1)] border-2 items-center gap-4 w-40 py-4 px-2 rounded-md bg-[rgba(252,233,255,1)]">
+            <div>
+              <h1 className="text-[8px]">Fintask</h1>
+              <h1 className="text-xs font-black">Developement</h1>
+            </div>
+
+            <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img
+                src={image2.src}
+                className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3"
+              />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3">
+                3+
+              </span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-6">
+            <h1 className="text-gray-400 text-xs">10:00 AM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="flex ml-36 -mt-8 border-[rgba(167,195,209,1)] border-2 items-center gap-4 w-40 py-2 px-2 rounded-xs bg-[rgba(225,251,254,1)]">
+            <div>
+              <h1 className="text-[8px]">Fintask</h1>
+              <h1 className="text-xs font-black">Ux copywrite</h1>
+            </div>
+
+            <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 pr-2 mt-12">
+            <h1 className="text-gray-400 text-xs">12:00 PM</h1>
+            <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+          </div>
+
+          <div className="w-[172px] ml-24 -mt-12 bg-[rgba(253,254,225,1)] rounded-xl py-2 px-2 space-y-1  border border-[rgba(248,255,31,1)]">
+            <div className="flex items-center gap-12 justify-between">
+              <p className="font-extralight text-gray-400 text-[8px]">Fintask</p>
+              <MoreHorizontal />
+            </div>
+
+            <div>
+              <h1 className="font-black text-xs">Web Visual Design</h1>
+            </div>
+
+            <div className="flex items-center gap-4 mt-2 justify-between">
+              <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+              <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+              <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+              <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
+            </div>
+
+              <div className="flex items-center">
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif">Visual</span>
+                <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif ml-0.5 mr-3">🏆</span>
+                <p><MessageCircle size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500 mr-2">5</p>
+                <p><Paperclip size={10} color="gray" /></p>
+                <p className="text-[7px] text-gray-500">3</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[rgba(0,0,0,0.03)] w-80 h-[395px] rounded-2xl absolute z-10 top-48 -left-8 -rotate-6"></div>
+      
+      {/* Second Card */}
+      <div className="w-96 absolute z-20 bg-[rgba(252,252,232,0.9)] mt-28 ml-8 pr-6 rounded-2xl shadow-lg transform rotate-[-1deg]">
+        <div className="opacity-70">
+          <div className="flex items-center pt-6 pb-4 px-6">
+            <div className="border-[rgba(52,199,89,1)] bg-[rgba(52,199,89,0.2)] border-4 w-5 h-5 rounded-full"></div>
+            <h1 className="text-[26px] font-black ml-2">In Progress</h1>
+          </div>
+          <div>
+            <p className="text-[19px] text-start text-gray-800 ml-12 mb-3.5">
+              Working on design updates
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Third Card */}
+      <div className="w-96 absolute z-10 bg-[rgba(252,252,232,0.8)] mt-32 ml-16 pr-6 rounded-2xl shadow-lg">
+        <div className="opacity-60">
+          <div className="flex items-center pt-6 pb-4 px-6">
+            <div className="border-[rgba(0,122,255,1)] bg-[rgba(0,122,255,0.2)] border-4 w-5 h-5 rounded-full"></div>
+            <h1 className="text-[26px] font-black ml-2">Up Next</h1>
+          </div>
+          <div>
+            <p className="text-[19px] text-start text-gray-800 ml-12 mb-3.5">
+              Review new features
+            </p>
           </div>
         </div>
       </div>
