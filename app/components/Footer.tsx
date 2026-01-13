cf90f5763c2756f8072cade196012bc586ca7172
IshimweArlene <ishimwearlene74@gmail.com>
2026-01-13 23:02:37 +0200

    changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Logo from '@/public/photos/logo.png'
+import linkedIn from '@/public/photos/linkedIn.svg'
+import facebook from '@/public/photos/facebook.svg'
+import twitter from '@/public/photos/twitter.svg'
 const Footer = () => {
   return (
-    <div className="flex px-24 items-start justify-start gap-24">
+    <div className="flex px-24 items-start justify-start mt-24 mb-12 gap-24">
       <div className="flex flex-col">
         <div className="flex gap-4 items-center">
           <Image src={Logo} alt="Logo" className="h-16 w-16"/>
@@ -23,7 +26,18 @@ const Footer = () => {
       </div>
       <div className="flex flex-col">
         <p className="text-2xl font-medium">Keep in touch</p>
-        <input type="text" placeholder="email address" className="bg-[rgba(250,250,250,1)] border-2 border-black/40  rounded-xl px-4 py-4 text-xl font-medium text-black/48~" />
+        <div className="flex">
+          <input type="text" placeholder="email address" className="outline-none bg-[rgba(250,250,250,1)] border-2 border-black/4  rounded-l-xl px-4 py-4 text-xl font-medium text-black/48" />
+          <button className="h-[62px] w-[151px] bg-[rgba(151,91,236,1)] text-white font-bold border-2 border-[rgba(115,69,179,1)] rounded-r-xl">Subscribe</button>
+        </div>
+        
+        <p className="font-medium text-2xl text-black">Follow us </p>
+        <div className="flex gap-4">
+          <Image src={linkedIn} alt="LinkedIn" />
+          <Image src={facebook} alt="facebook" />      
+          <Image src={twitter} alt="twitter" />
+        </div>
+
       </div>
     </div>
   );
