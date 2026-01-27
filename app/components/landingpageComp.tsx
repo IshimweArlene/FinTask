085749b91dbaf529a5d8439deb748e8b7a51859b
IshimweArlene <ishimwearlene74@gmail.com>
2026-01-27 20:04:25 +0200

    changes

--- app/components/landingpageComp.tsx
+++ app/components/landingpageComp.tsx
@@ -1,15 +1,29 @@
 'use client';
-import {Triangle, ChevronDown,  ChevronUp, Inbox, Search, Settings} from 'lucide-react';
-import {Users2, Settings2, InboxIcon, Clock} from 'lucide-react';
-import image1 from '@/public/photos/image1.png'
-import image2 from '@/public/photos/image2.png';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { useState } from 'react';
 
 const LandingComponent = () => {
  
   return (
-    <div>
-      
+    <div className='w-[1122px] h-[908px] bg-white'>
+      <div className='flex items-stretch '>
+        <div className='w-[287.51px] rounded-l-2xl h-[908.51px] bg-[rgba(240,239,234,0.56)]'>
+
+        </div>
+        <div className='px-14 py-12'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center text-black text-[14.03px] gap-2'>
+              <ArrowLeft size={12} color='black' />
+              <ArrowRight size={12} color='gray' />
+              <div className='bg-black/8 w-11 h-1.5 rounded-2xl'/>
+              /Landing
+            </div>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
