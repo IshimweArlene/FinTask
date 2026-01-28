ea5382899e658e8b9a3f8602779c625bbaa01436
IshimweArlene <ishimwearlene74@gmail.com>
2026-01-28 20:28:31 +0200

    changes

--- app/components/landingpageComp.tsx
+++ app/components/landingpageComp.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ArrowLeft, ArrowRight, Plus} from 'lucide-react';
+import { ArrowLeft, ArrowRight, Plus, Star, MoreHorizontal, MessageCircle, Paperclip, ChevronDown} from 'lucide-react';
 import CalenderBank from '@/public/photos/landingComp/CalendarBlank.svg'
 import Calendar from '@/public/photos/landingComp/CalendarBlank.png'
 import Clock from '@/public/photos/landingComp/Clock.svg'
@@ -10,9 +10,11 @@ import Trash from '@/public/photos/landingComp/Trash.svg'
 import Tray from '@/public/photos/landingComp/Tray.svg'
 import { useState } from 'react';
 import Image from 'next/image';
+import image1 from "@/public/photos/image1.png";
+import image2 from "@/public/photos/image2.png";
 
 const LandingComponent = () => {
- 
+  const [active, setActive] = useState<"day" | "week" | "month">("day");
   return (
     <div className='w-[1122px] h-[908px] bg-white'>
       <div className='flex items-stretch '>
@@ -47,21 +49,107 @@ const LandingComponent = () => {
               </div>              
             </div>
           </div>
-          <div className='flex gap-3 border-b-2 border-black/88'>
+          <div className='flex gap-6 border-b-2 border-black/88'>
             <div className='flex items-center gap-3'>
               <Image src={CalenderBank} alt='CalenderBank' />
               <div className='h-2.5 w-[111px] bg-[rgba(247,247,247,1)] rounded-2xl'/>
             </div>
             <div className='flex items-center gap-3'>
               <Image src={Table} alt='CalenderBank' />
-              <div className='h-2.5 w-[111px] bg-[rgba(247,247,247,1)] rounded-2xl'/>
+              <div className='h-2.5 w-[69px] bg-[rgba(247,247,247,1)] rounded-2xl'/>
             </div>
             <div className='flex items-center gap-3 text-black font-medium text-[18.71px] '>
               <Image src={Calendar} alt='CalenderBank' />
               Calendar View
             </div>
-            <div>
-              <Plus size={24.93} color='black' className='bg-black/4 p-1 shrink-0 rounded-full' />
+            <div className='flex items-center gap-5'>
+              <Plus size={24.93} color='black' className='bg-black/4 p-1 shrink-0 rounded-full ml-4' />
+              <Star size={15.58} color='black' fill='black' />
+              <div className='h-2.5 w-[58px] bg-[rgba(247,247,247,1)] rounded-2xl'/>
+              <MoreHorizontal size={24.93} color='black' />
+            </div>
+          </div>
+          <div className="bg-white p-3 rounded-r-2xl rounded-bl-2xl">
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <ChevronDown fill="black" size={18} className="rotate-270" />
+                <p className="text-[13px] font-bold">November 15</p>
+              </div>
+              <div className="bg-gray-300 w-fit flex items-center p-2 gap-3 rounded-xl">
+                <button onClick={() => setActive("day")} className={active === "day" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400" }>
+                  Day
+                </button>
+                <button onClick={() => setActive("week")} className={ active === "week" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400"}>
+                  Week
+                </button>
+                <button onClick={() => setActive("month")} className={ active === "month" ? "text-black font-semibold bg-white px-2 rounded-md" : "text-gray-400"}>
+                  Month
+                </button>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4 pr-2 mt-6">
+              <h1 className="text-gray-400 text-xs">08:00 AM</h1>
+              <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+            </div>
+
+            <div className="flex ml-[66px] -mt-2 border-[rgba(243,176,255,1)] border-2 items-center gap-4 w-40 py-4 px-2 rounded-md bg-[rgba(252,233,255,1)]">
+              <div>
+                <h1 className="text-[8px]">Fintask</h1>
+                <h1 className="text-xs font-black">Developement</h1>
+              </div>
+              <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+                <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+                <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+                <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3">3+ </span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4 pr-2 mt-6">
+              <h1 className="text-gray-400 text-xs">10:00 AM</h1>
+              <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+            </div>
+
+            <div className="flex ml-36 -mt-8 border-[rgba(167,195,209,1)] border-2 items-center gap-4 w-40 py-2 px-2 rounded-xs bg-[rgba(225,251,254,1)]">
+              <div>
+                <h1 className="text-[8px]">Fintask</h1>
+                <h1 className="text-xs font-black">Ux copywrite</h1>
+              </div>
+              <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+                <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+                <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+                <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3">3+</span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4 pr-2 mt-12">
+              <h1 className="text-gray-400 text-xs">12:00 PM</h1>
+              <div className="flex-1 h-[0.5px] bg-gray-300"></div>
+            </div>
+
+            <div className="w-[172px] ml-24 -mt-12 bg-[rgba(253,254,225,1)] rounded-xl py-2 px-2 space-y-1 border border-[rgba(248,255,31,1)]">
+              <div className="flex items-center gap-12 justify-between">
+                <p className="font-extralight text-gray-400 text-[8px]">Fintask</p>
+                <MoreHorizontal />
+              </div>
+              <div>
+                <h1 className="font-black text-xs">Web Visual Design</h1>
+              </div>
+              <div className="flex items-center gap-4 mt-2 justify-between">
+                <div className="relative flex items-center gap-2 bg-transparent rounded-full py-1 w-fit">
+                  <img src={image1.src} className="w-[17px] h-[17px] rounded-full" />
+                  <img src={image2.src} className="w-3.5 h-3.5 rounded-full border-white border-2 -ml-3" />
+                  <span className="text-[6px] flex justify-center items-center h-3.5 w-3.5 rounded-full border-white border font-semibold bg-white/20 backdrop-blur-md text-gray-700 -ml-3"> 3+ </span>
+                </div>
+                <div className="flex items-center">
+                  <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif">Visual</span>
+                  <span className="text-[7px] rounded-sm bg-gray-300 p-1 font-serif ml-0.5 mr-3">🏆</span>
+                  <p><MessageCircle size={10} color="gray" /></p>
+                  <p className="text-[7px] text-gray-500 mr-2">5</p>
+                  <p><Paperclip size={10} color="gray" /></p>
+                  <p className="text-[7px] text-gray-500">3</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
